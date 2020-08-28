b630b361b23e8e863784527a32360f053137f626
HeroTianTYJ <48871307+HeroTianTYJ@users.noreply.github.com>
2020-08-28 10:14:47 +0800

    修复：Linux下支付宝和微信支付sdk引入路径的问题

--- extend/pay/alipay/autoload.php
+++ extend/pay/alipay/autoload.php
@@ -2,6 +2,6 @@
 
 spl_autoload_register(function ($className) {
     if (strstr($className, 'alipay')) {
-        require __DIR__ . '/../' . $className . '.php';
+        require __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
     }
 });
--- extend/pay/wxpay/autoload.php
+++ extend/pay/wxpay/autoload.php
@@ -4,6 +4,6 @@ include __DIR__ . '/config.php';
 
 spl_autoload_register(function ($className) {
     if (strstr($className, 'wxpay')) {
-        require __DIR__ . '/../' . $className . '.php';
+        require __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
     }
 });
